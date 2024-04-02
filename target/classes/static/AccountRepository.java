6e8e1054ec2ec0a04a2e99a388917e89efa80afc
root <root@ip-172-31-40-233.ap-south-1.compute.internal>
2024-04-02 13:32:24 +0000

    Delete files

--- target/classes/static/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-public interface AccountRepository extends JpaRepository<Account, Long> {
-	    Optional<Account> findByAccountNumber(String accountNumber);
-	}
-
-	@Repository
-	public interface PolicyRepository extends JpaRepository<Policy, Long> {
-	    List<Policy> findByAccountNumber(String accountNumber);
-	}
-}
